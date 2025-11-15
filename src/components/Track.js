f32c9e3ec11411212fa2ddb2b22b570440b72c4f
Ashmita Chaki <ashmitachan@gmail.com>
2025-11-16 01:11:12 +0530

    Edited Track.js

--- src/components/Track.js
+++ src/components/Track.js
@@ -1,7 +1,6 @@
 function Track(){
     return(
         <div className="flex flex-col gap-3 w-full max-w-3xl">
-            {/* Track 1 */}
             <div className="grid grid-cols-4 gap-2">
             <div className="h-16 bg-[#648DB3] rounded shadow-md hover:bg-[#79b2c7] hover:shadow-lg "></div>
             <div className="h-16 bg-[#648DB3] rounded shadow-md hover:bg-[#79b2c7] hover:shadow-lg"></div>
